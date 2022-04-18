0d91618e3c704a3db6042d44e8c71c7ae8aa2851
Musa Azeezat <azeezahayomide@gmail.com>
2022-04-18 16:52:34 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,17 @@
  */
 int print_last_digit(int n)
 {	
-int last_digit = n % 10;
+	int last;
+	
+	last = n % 10;
 
-if (last_digit < 0)
-last_digit *= -1;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
 
-_putchar(last_digit + '0');
+	_putchar(last + '0');
 
-return (last_digit);
+	Return (last);
 }
+
