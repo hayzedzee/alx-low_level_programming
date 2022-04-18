3648effd74a205891f70083bca1d95915d1052c5
Musa Azeezat <azeezahayomide@gmail.com>
2022-04-18 05:52:50 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,7 @@
  * jack_bauer - Prints every minute of the day of 
  *              Jack Bauer, starting from 00:00 to 23:59.
  */
-void jack bauer(void)
+void jack_bauer(void)
 {
 int hour, minutes;
 
@@ -15,8 +15,8 @@ for (minute = 0; minute <= 59; minute++)
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
 _putchar(':');
-_putchar(minute / 10) + '0');
-_putchar(minute % 30) + '0');
+_putchar((minute / 10) + '0');
+_putchar((minute % 30) + '0');
 _putchar('\n');
 }
 }
