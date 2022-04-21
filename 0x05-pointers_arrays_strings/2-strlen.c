8076fdbd6a48fb33e04edf9c341dc04cd1b7bb12
Musa Azeezat <azeezahayomide@gmail.com>
2022-04-21 05:25:57 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strlen - Return the length of a string.
+ * _strlen - Returns the length of a string.
  * @str: The string to get the length of.
  *
  * Return: The length of @str.
  */
-size_t_strlen(const char *str)
+size_t _strlen(const char *str)
 {
 	size_t length = 0;
 
